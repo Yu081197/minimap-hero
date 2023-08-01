18d4713e194e77d5ca50d024e459ddd35dbdf61a
Dasong <81637903+Yu081197@users.noreply.github.com>
2023-08-02 02:02:55 +0900

    Test : 테스트 구현완료
    화면에 3개의 랜덤한 숫자가 나오고 0.5초가 지나면 각각 숫자가 줄어들고 5초 뒤에 다시 각각 랜덤한 숫자가 나오는 로직 구현 완료

--- src/__test__/test.jsx
+++ src/__test__/test.jsx
@@ -11,55 +11,86 @@ function App() {
   const [isNumber1Hidden, setIsNumber1Hidden] = useState(false);
   const [isNumber2Hidden, setIsNumber2Hidden] = useState(false);
   const [isNumber3Hidden, setIsNumber3Hidden] = useState(false);
+  const [interval1, setInterval1] = useState(null);
+  const [interval2, setInterval2] = useState(null);
+  const [interval3, setInterval3] = useState(null);
+  const [isNumber1Reset, setIsNumber1Reset] = useState(false);
+  const [isNumber2Reset, setIsNumber2Reset] = useState(false);
+  const [isNumber3Reset, setIsNumber3Reset] = useState(false);
 
   useEffect(() => {
-    const interval1 = setInterval(() => {
-      setNumber1((prevNumber) => prevNumber - 1);
-    }, 1000);
-
+    if (!isNumber1Hidden) {
+      const newInterval1 = setInterval(() => {
+        setNumber1((prevNumber) => prevNumber - 1);
+      }, 500);
+      setInterval1(newInterval1);
+    } else {
+      clearInterval(interval1);
+    }
     return () => clearInterval(interval1);
-  }, []);
+  }, [isNumber1Hidden]);
 
   useEffect(() => {
-    const interval2 = setInterval(() => {
-      setNumber2((prevNumber) => prevNumber - 1);
-    }, 1000);
-
+    if (!isNumber2Hidden) {
+      const newInterval2 = setInterval(() => {
+        setNumber2((prevNumber) => prevNumber - 1);
+      }, 500);
+      setInterval2(newInterval2);
+    } else {
+      clearInterval(interval2);
+    }
     return () => clearInterval(interval2);
-  }, []);
+  }, [isNumber2Hidden]);
 
   useEffect(() => {
-    const interval3 = setInterval(() => {
-      setNumber3((prevNumber) => prevNumber - 1);
-    }, 1000);
-
+    if (!isNumber3Hidden) {
+      const newInterval3 = setInterval(() => {
+        setNumber3((prevNumber) => prevNumber - 1);
+      }, 500);
+      setInterval3(newInterval3);
+    } else {
+      clearInterval(interval3);
+    }
     return () => clearInterval(interval3);
-  }, []);
+  }, [isNumber3Hidden]);
 
   useEffect(() => {
     // 숫자가 0이 되면 각각 해당 숫자만 초기화
-    if (number1 <= -1) {
+    if (number1 === 0 && !isNumber1Reset) {
       setIsNumber1Hidden(true);
+      setIsNumber1Reset(true);
       setTimeout(() => {
         setNumber1(getRandomNumber());
+        setIsNumber1Reset(false);
         setIsNumber1Hidden(false);
       }, 5000);
     }
-    if (number2 <= -1) {
+    if (number2 === 0 && !isNumber2Reset) {
       setIsNumber2Hidden(true);
+      setIsNumber2Reset(true);
       setTimeout(() => {
         setNumber2(getRandomNumber());
+        setIsNumber2Reset(false);
         setIsNumber2Hidden(false);
       }, 5000);
     }
-    if (number3 <= -1) {
+    if (number3 === 0 && !isNumber3Reset) {
       setIsNumber3Hidden(true);
+      setIsNumber3Reset(true);
       setTimeout(() => {
         setNumber3(getRandomNumber());
+        setIsNumber3Reset(false);
         setIsNumber3Hidden(false);
       }, 5000);
     }
-  }, [number1, number2, number3]);
+  }, [
+    number1,
+    number2,
+    number3,
+    isNumber1Reset,
+    isNumber2Reset,
+    isNumber3Reset,
+  ]);
 
   return (
     <div>
