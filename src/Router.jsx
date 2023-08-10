d3c5cc48a9635e540a3bea43fac1674edcba6454
Dasong <81637903+Yu081197@users.noreply.github.com>
2023-08-10 18:33:06 +0900

    Feat : 모바일 감지 추가

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
 
 import StartPage from "./page/StartPage/StartPage";
 import ScorePage from "./page/ScorePage/ScorePage";
@@ -12,11 +13,29 @@ import HowToPlay01 from "./page/HowToPlay/HowToPlay01";
 import HowToPlay02 from "./page/HowToPlay/HowToPlay02";
 import HowToPlay03 from "./page/HowToPlay/HowToPlay03";
 
+const StyledWrapper = styled.div`
+  color: #cdfafa;
+  font-size: 50px;
+  white-space: pre-line;
+`;
+
+function isMobileDevice() {
+  return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+}
+
+function MobileMessage() {
+  return (
+    <StyledWrapper>
+      <p>Minimap-Hero는 PC로만 플레이 가능합니다</p>
+    </StyledWrapper>
+  );
+}
+
 function routes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<StartPage />} />
+        {isMobileDevice() && <Route path="/" element={<MobileMessage />} />}
         <Route path="/how-to-play" element={<HowToPlay />} />
         <Route path="/how-to-play-01" element={<HowToPlay01 />} />
         <Route path="/how-to-play-02" element={<HowToPlay02 />} />
@@ -26,6 +45,7 @@ function routes() {
         <Route path="/ingameplay" element={<InGamePlay />} />
         <Route path="/map" element={<Map />} />
         <Route path="/ui" element={<UI />} />
+        {!isMobileDevice() && <Route path="/" element={<StartPage />} />}
       </Routes>
     </BrowserRouter>
   );
